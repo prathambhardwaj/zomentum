69273e97a8fca50edc43931ee90fe239cbc5c856
Pratham <prathambhardwaj81@gmail.com>
2020-08-30 00:06:48 +0530

    second commit

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
 
-$( '.friend-drawer--onhover' ).on( 'click',  function() {
+
+$( '.friend-list--onhover' ).on( 'click',  function() {
   
-  $( '.chat-bubble' ).hide('slow').show('slow');
+  $( '.chat-cloud' ).hide('slow').show('slow');
   
 });
-
-// Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
